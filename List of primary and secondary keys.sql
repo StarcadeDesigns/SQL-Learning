95b0e9630fefecb2e9f9e027a4f6dc4e0461c2d3
StarcadeDesigns <64881501+StarcadeDesigns@users.noreply.github.com>
2020-05-13 09:51:17 -0500

    Add files via upload

---
+++ List of primary and secondary keys.sql
@@ -0,0 +1,28 @@
+/*Table: Adventurer 
+Primary Key: ID
+Secondary Keys: 
+
+Table: AdventurerBalance
+Primary Key: ID
+Secondary Keys: AdventurerID
+
+Table: EmergencyContacts
+Primary Key: ID
+Secondary Keys: AdventurerID
+
+Table: Guild Suggestions
+Primary Key: ID
+Secondary Keys: AdventurerID
+
+Table: Items
+Primary Key: ID
+Secondary Keys: 
+
+Table: Orders 
+Primary Key: ID
+Secondary Keys:  ItemID,AdventurerID,StoreID
+
+Table: Stores	
+Primary Key: ID
+Secondary Keys: 
+*/
