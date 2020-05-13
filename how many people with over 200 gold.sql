a0f8fb502d5da48fe4ebb9bfdbf9f4e5b8462dee
StarcadeDesigns <64881501+StarcadeDesigns@users.noreply.github.com>
2020-05-13 09:50:48 -0500

    Update how many people with over 200 gold.sql

--- how many people with over 200 gold.sql
+++ how many people with over 200 gold.sql
@@ -1,5 +1,5 @@
 --this one is showing me each persons gold balance that has over 200 but I only need the count of people total
 
-select gold
+select count (gold)
 from AdventurerBalance
 where gold >= 200
