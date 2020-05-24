4654055e29abf0c1a5a0157d4fc57f414f7d0cce
matthewstarzyk18 <64881501+matthewstarzyk18@users.noreply.github.com>
2020-05-23 21:45:41 -0500

    Create TableReferenceForChad.sql

---
+++ TableReferenceForChad.sql
@@ -0,0 +1,23 @@
+--7 querys 7 results 7 tables
+
+select id,  name, race, class, Archtype, age, Gender, Background, MagicYN, MaxHP
+from adventurer
+
+select *
+from AdventurerBalance
+
+select *
+from EmergencyContacts
+
+select *
+from GuildSuggestions
+
+select *
+from Items
+
+select *
+from Orders
+
+select *
+from Stores
+
